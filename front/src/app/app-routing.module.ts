c60fcf0f04c913fd1340de83f50f12a18f7e22b9
aymen <aymen2255@hotmail.fr>
2024-07-21 17:58:43 +0200

    create header component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import {HomeComponent} from './pages/home/home.component';
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
 const routes: Routes = [
-  {path: '', component: HomeComponent},
   {path: 'themes', loadChildren: () => import('./features/theme/theme.module').then((m => m.ThemeModule))},
+  {path: '', component: HomeComponent},
 ];
 
 @NgModule({
--- front/src/app/core/core.module.ts
+++ front/src/app/core/core.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import {SharedModule} from "../shared/shared.module";
+import {RouterModule} from "@angular/router";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatListModule
   ],
   exports: [
     HeaderComponent
